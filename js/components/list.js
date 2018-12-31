8c5015439348ca494c50cc70331fe740df1f104b
WonYoungYun <asmar20288@naver.com>
2018-12-31 18:14:35 +0900

    update

--- js/components/list.js
+++ js/components/list.js
@@ -1,73 +1,71 @@
-const tag = '[list]'
+const tag = "[list]";
 
-const toDoList = document.querySelector('.todoList')
-const TODOS_LS = 'toDos'
+const toDoList = document.querySelector(".todoList");
+const TODOS_LS = "toDos";
 
-let toDos = []
+let toDos = [];
 
-function checkToDo(){
-    const check = event.target
-    console.dir(check.classList)
-    if(check.classList[0] !== 'checked'){
-        check.classList.add('checked')
-    } else{
-        check.classList.remove('checked')
-    }
-    console.log(check)
+function checkToDo() {
+  const check = event.target;
+  if (check.classList[0] !== "checked") {
+    check.classList.add("checked");
+  } else {
+    check.classList.remove("checked");
+  }
 }
-function deleteToDo(){
-    const btn = event.target
-    const li = btn.parentNode.parentNode
-    toDoList.removeChild(li)
-    const cleanToDos = toDos.filter(toDo=>{
-        return toDo.id !== parseInt(li.id)
-    })
-    toDos = cleanToDos
-    saveToDos()
+function deleteToDo() {
+  const btn = event.target;
+  const li = btn.parentNode.parentNode;
+  toDoList.removeChild(li);
+  const cleanToDos = toDos.filter(toDo => {
+    return toDo.id !== parseInt(li.id);
+  });
+  toDos = cleanToDos;
+  saveToDos();
 }
 
-function paintTodo(text){
-    const li = document.createElement('li')
-    const span = document.createElement('span')
-    const delBtn = document.createElement('span')
-    const eraser = document.createElement('i')
-    eraser.classList.add('fas')
-    eraser.classList.add('fa-eraser')
-    const newId = toDos.length +1;
-    delBtn.classList.add('delBtn')
-    span.innerText = text
-    delBtn.appendChild(eraser)
-    li.appendChild(span)
-    li.appendChild(delBtn)
-    span.addEventListener("click", checkToDo)
-    eraser.addEventListener("click", deleteToDo)
-    li.id = newId;
+function paintTodo(text) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  const delBtn = document.createElement("span");
+  const eraser = document.createElement("i");
+  eraser.classList.add("fas");
+  eraser.classList.add("fa-eraser");
+  const newId = toDos.length + 1;
+  delBtn.classList.add("delBtn");
+  span.innerText = text;
+  delBtn.appendChild(eraser);
+  li.appendChild(span);
+  li.appendChild(delBtn);
+  span.addEventListener("click", checkToDo);
+  eraser.addEventListener("click", deleteToDo);
+  li.id = newId;
 
-    toDoList.appendChild(li)
-    const toDoObj = {
-        text,
-        id: newId
-    }
-    toDos.push(toDoObj)
-    saveToDos()
+  toDoList.appendChild(li);
+  const toDoObj = {
+    text,
+    id: newId
+  };
+  toDos.push(toDoObj);
+  saveToDos();
 }
-function saveToDos(){
-    localStorage.setItem(TODOS_LS, JSON.stringify(toDos))
+function saveToDos() {
+  localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
 }
-function loadToDos(){
-    const loadedToDos = localStorage.getItem(TODOS_LS)
-    if(loadedToDos !== null){
-        const parseToDos = JSON.parse(loadedToDos)
-        parseToDos.forEach(toDo =>{
-            paintTodo(toDo.text)
-        })
-    }
+function loadToDos() {
+  const loadedToDos = localStorage.getItem(TODOS_LS);
+  if (loadedToDos !== null) {
+    const parseToDos = JSON.parse(loadedToDos);
+    parseToDos.forEach(toDo => {
+      paintTodo(toDo.text);
+    });
+  }
 }
-export default  {
-    paint(text){
-        paintTodo(text)
-    },
-    init(){
-        loadToDos()
-    }
-}
+export default {
+  paint(text) {
+    paintTodo(text);
+  },
+  init() {
+    loadToDos();
+  }
+};
--- js/components/user.js
+++ js/components/user.js
@@ -1,50 +1,49 @@
-const tag = '[user]'
+const tag = "[user]";
 
-const modal = document.querySelector(".modal")
-const form = modal.querySelector(".inputName")
-const input = form.querySelector("input")
-const modalBg = document.querySelector(".modal-bg")
-const userName = document.querySelector(".userName")
-const button = modal.querySelector('button')
+const modal = document.querySelector(".modal");
+const form = modal.querySelector(".inputName");
+const input = form.querySelector("input");
+const modalBg = document.querySelector(".modal-bg");
+const userName = document.querySelector(".userName");
+const button = modal.querySelector("button");
 
-const USER_NAME = "currentUser"
-const SHOW_CHANGE = "showing"
+const USER_NAME = "currentUser";
+const SHOW_CHANGE = "showing";
 
-function saveName(value){
-    localStorage.setItem(USER_NAME, value)
+function saveName(value) {
+  localStorage.setItem(USER_NAME, value);
 }
-function paintGreeting(value){
-    userName.style.opacity = "1"
-    userName.innerText = `Welcome! ${value}`
+function paintGreeting(value) {
+  userName.style.opacity = "1";
+  userName.innerText = `Welcome! ${value}`;
+  userName.addEventListener("click", askUserName);
 }
-function handleSubmit(e){
-    modal.classList.remove(SHOW_CHANGE)
-    modalBg.classList.remove(SHOW_CHANGE)
-    e.preventDefault()
-    const value = input.value
-    paintGreeting(value)
-    saveName(value)
+function handleSubmit(e) {
+  modal.classList.remove(SHOW_CHANGE);
+  modalBg.classList.remove(SHOW_CHANGE);
+  e.preventDefault();
+  const value = input.value;
+  paintGreeting(value);
+  saveName(value);
 }
-function askUserName(){
-    modal.classList.add(SHOW_CHANGE)
-    modalBg.classList.add(SHOW_CHANGE)
-    form.addEventListener("submit", handleSubmit)
-    button.addEventListener("click", handleSubmit)
-
-}
-
-function loadName(){
-    const currentUser = localStorage.getItem(USER_NAME)
-    if(currentUser === null){
-
-        askUserName()
-    } else{
-        paintGreeting(currentUser)
-    }
+function askUserName() {
+  modal.classList.add(SHOW_CHANGE);
+  modalBg.classList.add(SHOW_CHANGE);
+  form.addEventListener("submit", handleSubmit);
+  button.addEventListener("click", handleSubmit);
 }
 
-export default{
-    init(){
-        loadName()
-    }
+function loadName() {
+  const currentUser = localStorage.getItem(USER_NAME);
+  if (currentUser === null) {
+    askUserName();
+  } else {
+    paintGreeting(currentUser);
+  }
 }
+
+export default {
+  init() {
+    loadName();
+  }
+};
