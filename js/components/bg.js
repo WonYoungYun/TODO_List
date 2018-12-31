688b694b99e8e37d355a5eaf24237d014cd8fb2e
WonYoungYun <asmar20288@naver.com>
2018-12-31 18:42:39 +0900

    bg

--- js/components/bg.js
+++ js/components/bg.js
@@ -1,19 +1,20 @@
-const tag = '[background]'
+const tag = "[background]";
 
-const backgroundImg = document.getElementById("bgImage")
 const IMG_NUMBER = 3;
 
-function paintImage(imgNumber){
-    backgroundImg.src = `./img/${imgNumber +1}.jpg`
+function paintImage(imgNumber) {
+  const bg = `url(./img/${imgNumber + 1}.jpg) no-repeat center center fixed`;
+  console.log(bg);
+  document.body.style.background = `${bg}`;
 }
 
-function getRandom(){
-    const number = Math.floor(Math.random() * IMG_NUMBER)
-    return number
+function getRandom() {
+  const number = Math.floor(Math.random() * IMG_NUMBER);
+  return number;
 }
-export default{
-    init(){
-        const randomNumber = getRandom()
-        paintImage(randomNumber)
-    }
-}
+export default {
+  init() {
+    const randomNumber = getRandom();
+    paintImage(randomNumber);
+  }
+};
